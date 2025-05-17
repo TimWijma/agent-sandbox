aae4c6f4373c7612078ff0ebe0976aa93da61991
Tim Wijma <tim.wijma@gmail.com>
2025-05-17 19:53:29 +0200

    Initialized tailwind + shadcn

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
+
+<Button>
+    <span>Click me</span>
+</Button>
