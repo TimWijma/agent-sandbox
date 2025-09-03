bdf2be5a5e51af9289f6f56f26d266cd0d99055c
Tim Wijma <tim.wijma@gmail.com>
2025-09-03 10:10:54 +0200

    delete frontend

--- frontend/src/lib/stores/globalStore.ts
+++
@@ -1,9 +0,0 @@
-import type { ConversationDTO } from '$lib/DTO/Conversation';
-import { ChatManager } from '$lib/scripts/ChatManager';
-import { writable, type Writable } from 'svelte/store';
-
-export const BACKEND_URL = writable('http://localhost:8000');
-
-export const conversations: Writable<ConversationDTO[]> = writable([]);
-
-export const chatManager = new ChatManager('http://localhost:8000');
